a36e1c6dc4e83a907cb162bd729b97262e3f6238
i342202 <wiki.zhu@sap.com>
2021-09-16 14:10:57 +0800

    titanic

--- Titanic/titanic_analysis.py
+++ Titanic/titanic_analysis.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.tree import DecisionTreeClassifier
+
+import numpy as np
+from sklearn.model_selection import cross_val_score
 
 train_data = pd.read_csv('./data/train.csv')
 test_data = pd.read_csv('./data/test.csv')
@@ -18,3 +22,20 @@ features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
 train_features = train_data[features]
 train_labels = train_data['Survived']
 test_features = test_data[features]
+
+dvec = DictVectorizer(sparse=False)
+train_features = dvec.fit_transform(train_features.to_dict(orient='record'))
+
+clf = DecisionTreeClassifier(criterion='entropy')
+clf.fit(train_features, train_labels)
+
+# test_features = dvec.transform(test_features.to_dict(orient='record'))
+# # 决策树预测
+# pred_labels = clf.predict(test_features)
+
+# 得到决策树准确率
+acc_decision_tree = round(clf.score(train_features, train_labels), 6)
+print(u'score准确率为 %.4lf' % acc_decision_tree)
+
+# 使用K折交叉验证 统计决策树准确率
+print(u'cross_val_score准确率为 %.4lf' % np.mean(cross_val_score(clf, train_features, train_labels, cv=10)))
